942b79b8dee618281cd77cfe493a32d785cefe51
Voxys <yves.the.svecht@gmail.com>
2023-08-28 23:37:36 +0200

    amogus

---
+++ Unity Collab/Assets/Scripts/IsometricCharacterRenderer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricCharacterRenderer : MonoBehaviour
+{
+
+    public static readonly string[] staticDirections = {"Static N", "Static NE", "Static E", "Static SE", "Static S", "Static SW", "Static W", "Static NW"};
+    public static readonly string[] runDirections = {"Run N", "Run NE", "Run E", "Run SE", "Run S", "Run SW", "Run W", "Run NW"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
